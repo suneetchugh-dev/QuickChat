fce4741a95384df384f4f564a166dcb33a36e3dd
suneetchugh-dev <valorise078gramming@gmail.com>
2025-11-25 19:57:22 +0530

    Shifted to VITE due to CRA dependency issues & Installed Basic Packages And Got To Set-Up Routing

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+const theme = extendTheme({});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
